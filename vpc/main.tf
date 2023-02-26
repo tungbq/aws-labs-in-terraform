1ba991d5088a20476458d8841696003f4fe1176f
Tung Bui <tung.bquang@gmail.com>
2023-02-26 23:15:20 +0700

    init vpc

---
+++ vpc/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
